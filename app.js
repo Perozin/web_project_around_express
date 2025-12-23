674b8975fdc3d25eaca95fb182d85e9dddc0cf3c
MarcioPerusin <marcioperozin@gmail.com>
2025-12-23 15:01:38 -0300

    update: updates app.js

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
+// app.js
 const express = require('express');
-
 const app = express();
+
+const usersRouter = require('./routes/users');
+const cardsRouter = require('./routes/cards');
+
 const { PORT = 3000 } = process.env;
 
-app.get('/', (req, res) => {
-  res.send('Servidor Express está rodando 🚀');
+app.use('/users', usersRouter);
+app.use('/cards', cardsRouter);
+
+// rota inexistente
+app.use((req, res) => {
+  res.status(404).send({ message: 'Recurso requisitado não encontrado' });
 });
 
 app.listen(PORT, () => {
