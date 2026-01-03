e849e8c5f285e40cc86d5e172c9fe340da8d92ef
MarcioPerusin <marcioperozin@gmail.com>
2026-01-03 16:57:51 -0300

    bugfix: fixeds the routes/users.js

--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,13 @@
-// routes/users.js
 const router = require('express').Router();
-const fs = require('fs');
 const path = require('path');
+const readJsonFile = require('../utils/readJsonFile');
 
 const usersPath = path.join(__dirname, '../data/users.json');
 
 // GET /users
 router.get('/', (req, res) => {
-  fs.readFile(usersPath, 'utf8', (err, data) => {
-    if (err) {
-      res.status(500).send({ message: 'Ocorreu um erro no servidor' });
-      return;
-    }
-
-    res.send(JSON.parse(data));
+  readJsonFile(usersPath, res, (users) => {
+    res.send(users);
   });
 });
 
@@ -21,13 +15,7 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   const { id } = req.params;
 
-  fs.readFile(usersPath, 'utf8', (err, data) => {
-    if (err) {
-      res.status(500).send({ message: 'Ocorreu um erro no servidor' });
-      return;
-    }
-
-    const users = JSON.parse(data);
+  readJsonFile(usersPath, res, (users) => {
     const user = users.find((item) => item._id === id);
 
     if (!user) {
@@ -40,3 +28,47 @@ router.get('/:id', (req, res) => {
 });
 
 module.exports = router;
+
+// // funcionando plenamente
+// // routes/users.js
+// const router = require('express').Router();
+// const fs = require('fs');
+// const path = require('path');
+
+// const usersPath = path.join(__dirname, '../data/users.json');
+
+// // GET /users
+// router.get('/', (req, res) => {
+//   fs.readFile(usersPath, 'utf8', (err, data) => {
+//     if (err) {
+//       res.status(500).send({ message: 'Ocorreu um erro no servidor' });
+//       return;
+//     }
+
+//     res.send(JSON.parse(data));
+//   });
+// });
+
+// // GET /users/:id
+// router.get('/:id', (req, res) => {
+//   const { id } = req.params;
+
+//   fs.readFile(usersPath, 'utf8', (err, data) => {
+//     if (err) {
+//       res.status(500).send({ message: 'Ocorreu um erro no servidor' });
+//       return;
+//     }
+
+//     const users = JSON.parse(data);
+//     const user = users.find((item) => item._id === id);
+
+//     if (!user) {
+//       res.status(404).send({ message: 'ID do usuário não encontrado' });
+//       return;
+//     }
+
+//     res.send(user);
+//   });
+// });
+
+// module.exports = router;
