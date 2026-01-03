959e849eea20fe25953286113e14483ad82ba817
MarcioPerusin <marcioperozin@gmail.com>
2026-01-03 16:56:56 -0300

    bugfix: fixeds the routes/cards.js

--- routes/cards.js
+++ routes/cards.js
@@ -1,20 +1,36 @@
-// routes/cards.js
 const router = require('express').Router();
-const fs = require('fs');
 const path = require('path');
+const readJsonFile = require('../utils/readJsonFile');
 
 const cardsPath = path.join(__dirname, '../data/cards.json');
 
 // GET /cards
 router.get('/', (req, res) => {
-  fs.readFile(cardsPath, 'utf-8', (err, data) => {
-    if (err) {
-      res.status(500).send({ message: 'Ocorreu um erro no servidor' });
-      return;
-    }
-
-    res.send(JSON.parse(data));
+  readJsonFile(cardsPath, res, (cards) => {
+    res.send(cards);
   });
 });
 
 module.exports = router;
+
+// // Funcionando plenamente
+// // routes/cards.js
+// const router = require('express').Router();
+// const fs = require('fs');
+// const path = require('path');
+
+// const cardsPath = path.join(__dirname, '../data/cards.json');
+
+// // GET /cards
+// router.get('/', (req, res) => {
+//   fs.readFile(cardsPath, 'utf-8', (err, data) => {
+//     if (err) {
+//       res.status(500).send({ message: 'Ocorreu um erro no servidor' });
+//       return;
+//     }
+
+//     res.send(JSON.parse(data));
+//   });
+// });
+
+// module.exports = router;
